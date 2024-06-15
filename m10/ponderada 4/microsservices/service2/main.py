af05e0bd86ac016c13372e81bf162955692f0f82
antonio-ang2 <antonio.teixeira@sou.inteli.edu.br>
2024-06-15 15:06:51 -0300

    feat: service2

--- m10/ponderada 4/microsservices/service2/main.py
+++ m10/ponderada 4/microsservices/service2/main.py
@@ -5,7 +5,7 @@ app = FastAPI()
 
 @app.get("/service2")
 async def read_root():
-    return {"message": "Hello, world! from service 2"}
+    return {"message": "Murilo, isso é um pedido de socorro. Por favor, me dá 10 nessa ponderada, estou desesperado!"}
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8002)
